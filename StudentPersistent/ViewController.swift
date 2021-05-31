d4333997cddd47dca21f38303fa6830ad4b637d8
MedNizar <rockstarhard4@gmail.com>
2021-05-31 16:23:39 +0100

    Initial Commit

---
+++ StudentPersistent/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StudentPersistent
+//
+//  Created by MNizar on 5/31/21.
+//  Copyright © 2021 MNizar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
